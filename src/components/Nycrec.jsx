473a3d0b9dcf2f4710902440c6aace80a151c508
tifflawl <tifflawl@yahoo.com>
2025-01-30 19:07:05 -0800

    fix css

--- src/components/Nycrec.jsx
+++ src/components/Nycrec.jsx
@@ -1,7 +1,7 @@
 const Nycrec = ({ nycrec }) => {
   return (
-    <li className="Nycrec" id={nycrec.name}>
-      <div className="Nycrec-info">
+    <li className="nycrec" id={nycrec.name}>
+      <div className="nycrec-info">
         <h3>{nycrec.name}</h3>
         <img src={nycrec.image.exterior} />
         <h5>Borough: {nycrec.location.borough}</h5>
