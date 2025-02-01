809e3ab38402bcf0df9595138f3838cb1af16976
tifflawl <tifflawl@yahoo.com>
2025-01-31 19:40:09 -0800

    remove borders and change bg color

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,8 +3,6 @@ import Nycrec from './Nycrec'
 const MainContent = ({ data }) => {
   return (
     <div className="main">
-      <h1>New York City 🍎</h1>
-      <h2>tiff's faves in nyc!</h2>
       <ul>
         {data.nycrec.map((nycrec) => (
           <Nycrec nycrec={nycrec} />
